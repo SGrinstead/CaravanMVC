80d1f9b4f0bf3fa6d1c8bac07839d91139f4ade0
SGrinstead <seth.grinstead3299@gmail.com>
2023-08-21 14:23:11 -0700

    new and create

---
+++ CaravanMVC/Controllers/PassengersController.cs
@@ -0,0 +1,42 @@
+﻿using CaravanMVC.DataAccess;
+using CaravanMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CaravanMVC.Controllers
+{
+	public class PassengersController : Controller
+	{
+		private readonly CaravanMvcContext _context;
+
+		public PassengersController(CaravanMvcContext context)
+		{
+			_context = context;
+		}
+
+		[Route("/wagons/{wagonId:int}/passengers/new")]
+		public IActionResult New(int wagonId)
+		{
+			var wagon = _context.Wagons
+				.Where(w => w.Id == wagonId)
+				.Include(w => w.Passengers)
+				.FirstOrDefault();
+
+			return View(wagon);
+		}
+
+		[HttpPost]
+		[Route("/wagons/{wagonId:int}/passengers")]
+		public IActionResult Create(int wagonId, Passenger passenger)
+		{
+			var wagon = _context.Wagons
+				.Where(w => w.Id == wagonId)
+				.Include(w => w.Passengers)
+				.FirstOrDefault();
+			wagon.Passengers.Add(passenger);
+			_context.SaveChanges();
+
+			return Redirect($"/wagons/{wagonId}");
+		}
+	}
+}
